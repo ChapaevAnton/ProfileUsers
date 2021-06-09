734000b090d9aaf46db4fdf7f94a6cc200675db2
ChapaevAnton <virsus2009@gmail.com>
2021-06-09 14:23:56 +0700

    add fragment

--- app/src/main/java/com/example/profileusers/profile/UserProfileFragment.java
+++ app/src/main/java/com/example/profileusers/profile/UserProfileFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.profileusers.R;
@@ -13,8 +16,26 @@ import com.example.profileusers.R;
 
 public class UserProfileFragment extends Fragment {
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.user_profile_fragment, container, false);
+
+        View fragmentView = inflater.inflate(R.layout.user_profile_fragment, container, false);
+        if (fragmentView != null) {
+            ImageView userPhoto = fragmentView.findViewById(R.id.user_photo);
+            Button selectPhotoButton = fragmentView.findViewById(R.id.select_photo_button);
+
+            selectPhotoButton.setOnClickListener(view -> {
+
+            });
+        }
+        return fragmentView;
     }
+
 }
