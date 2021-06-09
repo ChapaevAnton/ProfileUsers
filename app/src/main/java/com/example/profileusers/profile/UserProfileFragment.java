5be8880cf961a3fbf36d4beec707c1aacf8a877b
ChapaevAnton <virsus2009@gmail.com>
2021-06-09 14:43:37 +0700

    add selectPhoto

--- app/src/main/java/com/example/profileusers/profile/UserProfileFragment.java
+++ app/src/main/java/com/example/profileusers/profile/UserProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.profileusers.profile;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +17,7 @@ import com.example.profileusers.R;
 
 
 public class UserProfileFragment extends Fragment {
-
+    private ImageView userPhoto;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,14 +30,48 @@ public class UserProfileFragment extends Fragment {
 
         View fragmentView = inflater.inflate(R.layout.user_profile_fragment, container, false);
         if (fragmentView != null) {
-            ImageView userPhoto = fragmentView.findViewById(R.id.user_photo);
+            userPhoto = fragmentView.findViewById(R.id.user_photo);
             Button selectPhotoButton = fragmentView.findViewById(R.id.select_photo_button);
 
             selectPhotoButton.setOnClickListener(view -> {
-
+                selectPhoto();
             });
         }
         return fragmentView;
+    }
+
+
+    void selectPhoto() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), 200);
+    }
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == -1) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == 200) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    userPhoto.setImageURI(selectedImageUri);
+                }
+            }
+        }
     }
 
 }
